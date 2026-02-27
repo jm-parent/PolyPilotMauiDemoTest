3127de6dc17dd5810790cc8b859ee621f790ff23
Jean-Marie PARENT <39386863+jm-parent@users.noreply.github.com>
2026-02-27 11:50:47 +0100

    Ajoutez des fichiers projet.

---
+++ PolyPilotMauiDemoTest/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace PolyPilotMauiDemoTest
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PolyPilotMauiDemoTest/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PolyPilotMauiDemoTest
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
